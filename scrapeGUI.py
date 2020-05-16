c7ffb8bffa1224f0da315242e907eae9848c64d5
Michael Boimistruck <michaelb_38@hotmail.com>
2020-05-16 16:00:14 -0400

    many changes to the process of obtaining prices
    note: Need to rewrite the entire process to simplify it and have less "scraping".
    Also, need to simplify an order in which we determine what type of prices we want to store, e.g. NM only, what happens if there's only SP in stock, what happens if theres no buylist whatsoever?

--- scrapeGUI.py
+++ scrapeGUI.py
@@ -2,7 +2,7 @@ import os
 import os.path
 import tkinter
 from datetime import date
-from tkinter import Tk, Listbox, END
+from tkinter import Tk, Listbox, END, messagebox
 from tkinter import ttk
 
 import requests
@@ -27,19 +27,23 @@ class DeckMGT(ttk.Frame):
         self.labelTop = ttk.Label(self.parent, text="View Card History")
         self.labelTop.pack()
 
-        cardNames = []  # populate a list of decklists
+        cardNames = []  # populate a list of cards
         if not os.listdir("Cards/"):
             cardNames.append("No cards in system")
         else:
             for each in os.listdir("Cards"):
                 cardNames.append(each.replace(".txt", ""))
-        self.cardNameComboBox = ttk.Combobox(self.parent, state="readonly", values=list(set(cardNames)),
-                                             postcommand=self.updateList, width=25)
+        self.cardNameComboBox = ttk.Combobox(self.parent, values=cardNames,
+                                             postcommand=self.updateList, width=25)  # state="readonly
+
         self.cardNameComboBox.pack()
         self.cardNameComboBox.current(0)
 
         self.cardHistoryBtn = ttk.Button(self.parent, text="View", command=self.generateCardHistoryGUI)
         self.cardHistoryBtn.pack()
+
+        self.deleteCardBtn = ttk.Button(self.parent, text="Delete Card", command=self.removeCard)
+        self.deleteCardBtn.pack()
 
         self.cardHistoryBtn = ttk.Button(self.parent, text="Generate Price Report", command=self.generatePriceReport)
         self.cardHistoryBtn.pack()
@@ -65,31 +69,56 @@ class DeckMGT(ttk.Frame):
 
     def generatePriceReport(self):
         storedReports = []
+        buylistURLs = []
         listbox = Listbox(Tk())
         listbox.configure(width=75, height=20)
         listbox.winfo_toplevel().title("Price Report")
         listbox.configure()
         listbox.pack()
+        looptest = 0
 
         for each in open("URLs.txt"):
             print(each)
+            buylistURLs.append(each.replace("catalog", "buylist"))
+            if(
+                BeautifulSoup(requests.get(url=buylistURLs[looptest], headers=headers).content, "html.parser").find(
+                    'form', attrs={"class": "add-to-cart-form", "data-variant": "NM-Mint, English"}) is None):
+                buylistPrice = 0
+            else:
+                buylistPrice = BeautifulSoup(requests.get(url=buylistURLs[looptest], headers=headers).content, "html.parser").find(
+                    'form', attrs={"class": "add-to-cart-form", "data-variant": "NM-Mint, English"}).get_text()[
+                7:].split("/", 1)[0].strip()
+            looptest += 1
             soup = BeautifulSoup(requests.get(url=each, headers=headers).content, "html.parser")
             cardName = soup.find('h1', attrs={"class": "title"}).get_text().replace('/', '')
             if (soup.find('span', attrs={"class": "variant-short-info"}).get_text() == "Out of stock."):
                 price = soup.find('span', attrs={"class": "price no-stock"}).get_text()[5:]
             else:
-                price = soup.find('span', attrs={"class": "regular price"}).get_text()[5:]
-
+                if (soup.find('form', attrs={"class": "add-to-cart-form", "data-variant": "NM-Mint, English",
+                                             "data-name": cardName}) is None):
+                    price = (
+                        soup.find('form',
+                                  attrs={"class": "add-to-cart-form"}).get_text()[
+                        7:].split("/", 1)[0].rstrip())
+                else:
+                    price = (
+                        soup.find('form',
+                                  attrs={"class": "add-to-cart-form", "data-variant": "NM-Mint, English",
+                                         "data-name": cardName}).get_text()[
+                        7:].split("/", 1)[0].rstrip())
+            print(price)
+            print(buylistPrice)
             if (os.path.exists("Cards/" + cardName + ".txt")):
                 eachLastLine = open("Cards/" + cardName + ".txt", "r+", encoding="utf-8").readlines()[-1].split("|", 1)[
                     0].strip()
             if (eachLastLine != price):
                 storedReports.append(cardName + " changed price from " + str(eachLastLine) + " to " + price)
                 open("Cards/" + cardName + ".txt", "a", encoding="utf-8").write(
-                    price + " | " + str(date.today()) + "\n")
-            else:
+                    price + " | Buylist (cash): " + buylistPrice + " | Credit: " + (
+                        str(float(buylistPrice) * 1.25)) + " | " + str((date.today())) + "\n")
+            """else:
                 eachLastLine = open("Cards/" + cardName + ".txt", "w", encoding="utf-8").write(
-                    price + " | " + str(date.today()) + "\n")
+                    price + " | " + str(date.today()) + "\n") #overwrites file if same price is met"""
 
         if (len(storedReports) == 0):
             listbox.insert(END, "NO CHANGES!")
@@ -99,24 +128,66 @@ class DeckMGT(ttk.Frame):
 
     def generateNewCard(self):
         soup = BeautifulSoup(requests.get(url=self.newURLEntry.get(), headers=headers).content, "html.parser")
+        cardName = soup.find('h1', attrs={"class": "title"}).get_text().replace('/', '')
+        buylistURLs = self.newURLEntry.get().replace("catalog", "buylist")
+        # buylistPrice = (BeautifulSoup(requests.get(url=buylistURLs, headers=headers).content, "html.parser").find('span', attrs={"class": "regular price"}).get_text()[5:].split("/", 1)[0].rstrip())
+        if (soup.find('form', attrs={"class": "add-to-cart-form", "data-variant": "NM-Mint, English",
+                                     "data-name": cardName}) is None):
+            buylistPrice = (
+                soup.find('form',
+                          attrs={"class": "add-to-cart-form"}).get_text()[
+                7:].split("/", 1)[0].rstrip())
+        else:
+            buylistPrice = (
+                soup.find('form',
+                          attrs={"class": "add-to-cart-form", "data-variant": "NM-Mint, English",
+                                 "data-name": cardName}).get_text()[
+                7:].split("/", 1)[0].rstrip())
+        """buylistPrice = BeautifulSoup(requests.get(url=buylistURLs, headers=headers).content, "html.parser").find('form',
+                                                                                                                 attrs={
+                                                                                                                     "class": "add-to-cart-form",
+                                                                                                                     "data-variant": "NM-Mint, English",
+                                                                                                                     "data-name": cardName}).get_text()[
+                       7:].split("/", 1)[0].rstrip()"""
+
         newCard = open("Cards/" + soup.find('h1', attrs={"class": "title"}).get_text().replace('/', '') + ".txt", "w")
         if (soup.find('span', attrs={"class": "variant-short-info"}).get_text() == "Out of stock."):
             newCard.write(
-                soup.find('span', attrs={"class": "price no-stock"}).get_text()[5:] + " | " + str(date.today()) + "\n")
+                soup.find('span', attrs={"class": "price no-stock"}).get_text()[
+                5:] + " | Buylist (cash): " + buylistPrice + " | Credit: " + (
+                    str(float(buylistPrice) * 1.25)) + " | " + str((date.today())) + "\n")
         else:
-            newCard.write(
-                soup.find('span', attrs={"class": "regular price"}).get_text()[5:] + " | " + str(date.today()) + "\n")
+            if (soup.find('form', attrs={"class": "add-to-cart-form", "data-variant": "NM-Mint, English",
+                                         "data-name": cardName}) is None):
+                newCard.write(
+                    soup.find('form',
+                              attrs={"class": "add-to-cart-form"}).get_text()[
+                    7:].split("/", 1)[0].rstrip() + " | Buylist (cash): " + buylistPrice + " | Credit: " + (
+                        str(float(buylistPrice) * 1.25)) + " | " + str((date.today())) + "\n")
+            else:
+                newCard.write(
+                    soup.find('form',
+                              attrs={"class": "add-to-cart-form", "data-variant": "NM-Mint, English"}).get_text()[
+                    7:].split("/", 1)[0].rstrip() + " | Buylist (cash): " + buylistPrice + " | Credit: " + (
+                        str(float(buylistPrice) * 1.25)) + " | " + str((date.today())) + "\n")
         newCard = open("URLs.txt", "a")
         newCard.write(self.newURLEntry.get() + "\n")
         newCard.close()
         self.newURLEntry.delete(0, END)
 
     def updateList(self):
-        cardNames = []  # populate a list of decklists
+        cardNames = []  # populate a list of cards
         for each in os.listdir("Cards"):
             cardNames.append(each.replace(".txt", ""))
 
-        self.cardNameComboBox.configure(values=list(set(cardNames)))  # updates decklist combobox on action
+        self.cardNameComboBox.configure(values=list(set(cardNames)))  # updates card combobox on action
+
+    def removeCard(self):
+        confirmationBtn = messagebox.askquestion("Delete Card", "Are you sure you want to delete this?",
+                                                 icon="warning")
+        if confirmationBtn == "yes":
+            os.remove("Cards/" + self.cardNameComboBox.get() + ".txt")
+            self.updateList()
 
     def exitApplication(self):
         root.destroy()
